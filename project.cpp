2a38def5959b206e2c8b32d850b1b5431e84f246
Abhishek Dantale <abhishekdantale@Abhisheks-MacBook-Pro.local>
2019-09-15 23:20:56 +0530

    Third Commit

--- project.cpp
+++ project.cpp
@@ -6,6 +6,9 @@
 using namespace std;
 
 
+void menu();
+void writeToPaxFile(Passenger);
+string paxFileName = "paxfile.dat";
 
 enum RouteType {
 	longRoute,shortRoute
@@ -161,6 +164,7 @@ class B737 : public Planes {
 class Route1:B737,A320,AirIndia {
 	vector<string> routeNos;
 	vector<string> routeToFrom;
+	vector<int> ticketsAvailable;
 
 	public:
 
@@ -174,10 +178,14 @@ class Route1:B737,A320,AirIndia {
 			while((ch=getchar())!='\n');
 			routeNos.resize(noOfRoutesServed);
 			routeToFrom.resize(noOfRoutesServed);
+			ticketsAvailable.resize(noOfRoutesServed);
 
 			for(int i=0;i<noOfRoutesServed;i++) {
-				getline(cin,routeNos[i]);
-				getline(cin,routeToFrom[i]);
+				cout<<"\nEnter the route Number : ";getline(cin,routeNos[i]);
+				cout<<"\nEnter the route path : ";getline(cin,routeToFrom[i]);
+				cout<<"\nEnter the total number of tickets available for this route : ";cin>>ticketsAvailable[i];
+				char ch;
+				while((ch=getchar())!='\n');
 			}
 		}
 
@@ -187,9 +195,9 @@ class Route1:B737,A320,AirIndia {
 			cout<<"\nThe Airline data is : ";
 			AirIndia::putBasicData();
 			cout<<"\nThe data of the routes is : ";
-			cout<<"\nRoute Number\tRoutle Name :: ";
+			cout<<"\nRoute Number\tRoutle Name\tTickets Available :: ";
 			for(int i=0;i<noOfRoutesServed;i++){
-				cout<<"\n"<<routeNos[i]<<"\t"<<routeToFrom[i];
+				cout<<"\n"<<routeNos[i]<<"\t"<<routeToFrom[i]<<"\t"<<ticketsAvailable[i];
 			}
 		}
 };
@@ -265,10 +273,111 @@ class Route3:B737,A320,Vistara {
 	}
 };
 
+class Passenger {
+	string name;
+	int age;
+	bool ticketBooked;
+	string routeBooked;
+	string uid;
+
+public:
+
+	void getPassengerData() {
+		cout<<"\nEnter your name : ";
+		getline(cin,name);
+		cout<<"\nEnter your age : ";
+		cin>>age;
+		char ch;
+		while((ch=getchar())!='\n');
+		cout<<"\nEnter the route you want the ticket for : ";
+		string neededRoute;
+		getline(cin,neededRoute);
+		cout<<"\nEnter the unique id you have been issued by the GoI : ";
+		getline(cin,uid);
+		routeBooked = neededRoute;
+
+	}
+
+	void putPassengerData() {
+		cout<<"\nName of the passenger : ";
+		cout<<name;
+		cout<<"\nAge of the passenger : ";
+		cout<<"age";
+		cout<<"\nPassenger travel route : "<<routeBooked;
+		cout<<"\nUnique Id : "<<uid<<endl;
+	}
+};
+
+//Route1 route1;
+// Route2 route2;
+// Route3 route3;
+
+void adminMenu(){
+	// cout<<"\nWhich airline's data you want to access : ";
+	// cout<<"\n1.AirIndia\n2.Indigo\n3.Vistara";
+	// int ch;
+	// cin>>ch;
+	// char _ch;
+	// while((_ch=getchar())!='\n');
+	// switch(ch){
+	// 	case 1: route1.getBasicData();break;
+	// 	case 2: route2.getBasicData();break;
+	// 	case 3: route3.getBasicData();break;
+	// 	default:cout<<"\nWrong Choice : ",adminMenu();break;
+	// }
+}
+
+void adminSeeDataMenu() {
+	cout<<"\nWhich airline's data you want to access : ";
+	cout<<"\n1.AirIndia\n2.Indigo\n3.Vistara";
+	int ch;
+	cin>>ch;
+	char _ch;
+	while((_ch=getchar())!='\n');
+
+	switch(ch){
+		//case 1:route1.putData();break;
+		// case 2:route2.putData();break;
+		// case 3:route3.putData();break;
+		default:cout<<"\nWrong choice",adminSeeDataMenu();break;
+	}
+}
+
+void userMenu() {
+	cout<<"\n1.Book Ticket"<<"\n2.View Ticket Details\n3.Exit";
+	int ch;
+	cin>>ch;
+	char _ch;
+	while((_ch=getchar())!='\n');
+	Passenger passenger;
+	switch(ch){
+		case 1:passenger.getPassengerData(),userMenu();break;
+		case 2:passenger.putPassengerData(),userMenu();break;
+		case 3:menu();break;
+		default:cout<<"\nWrong choice",userMenu();break;
+	}
+}
+
+void menu() {
+	cout<<"\n1.ADMIN MAKE CHANGE MENU"<<"\n2.USER MENU"<<"\n3.ADMIN SEE DATA MENU\n4.Exit";
+	int ch;
+	cin>>ch;
+	switch(ch){
+		case 1:adminMenu();break;
+		case 2:userMenu();break;
+		case 3:adminSeeDataMenu();break;
+		case 4:break;
+		default:cout<<"\nWrong choice",menu();break;
+	}
+}
+
+
+void writeToPaxFile(Passenger passenger){
+
+}
 
 int main() {
-	Route2 route;
-	route.putData();
+	menu();
 
 	return(0);
 
